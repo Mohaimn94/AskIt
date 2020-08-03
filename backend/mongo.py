5299c061a0f26c44b80ba8a43117a44dc527a4fe
Mohaimn <59797727+Mohaimn94@users.noreply.github.com>
2020-08-03 04:37:20 +0200

    proc

--- backend/mongo.py
+++ backend/mongo.py
@@ -6,6 +6,7 @@ from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager 
 from flask_jwt_extended import create_access_token
+import os
 
 app = Flask(__name__)
 
